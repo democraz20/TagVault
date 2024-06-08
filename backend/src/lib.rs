3d5e43e7b9ee66b19ff0ae8a3105e1fc0c2fe528
democraz20 <mozartrasig@gmail.com>
2024-06-08 08:48:03 +0000

    some stuff

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,7 +1,18 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct savefile {
-    tags: Vec<String>
+    //space for more metadata
+    tags: Vec<tag>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct tag {
+    tagname: String, 
+    filelist: Vec<String>
+}
+
+pub fn get_files_from_tags(tags: Vec<String>t) -> Vec<String> {
+
 }
