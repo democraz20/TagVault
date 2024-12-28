589182ecf28baf541849ff3e2beca5eacb90eda1
democraz20 <mozartrasig@gmail.com>
2024-12-29 03:54:43 +0700

    uhhhhbboiler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Write};
+
+fn main() -> Result<(), Box<dyn std::error::Error>>{
+    //cmdline mode
+    println!("No args passed, running in cmdline mode");
+    loop {
+        let mut cmdin = String::new();
+        print!(">");
+        io::stdin().read_line(&mut cmdin)?;
+        cmdin = cmdin.trim().to_string();
+        dbg!(&cmdin);
+        if cmdin.contains("exit") {
+            break;
+        }
+        io::stdout().flush()?;
+    }
+    Ok(())
 }
+
+fn store() {}
+fn get() {}
+fn update() {}
+fn del() {}
+//display and query seperated somehow
